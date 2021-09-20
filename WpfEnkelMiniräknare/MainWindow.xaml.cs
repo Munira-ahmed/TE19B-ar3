af0cb8795a15f4a3abfc218e4c0c91fd2a375633
Mona Lisa <munira.ahmed@elev.ga.ntig.se>
2021-09-20 12:12:52 +0200

    miniräknaren & uppgift 5 genomgång

--- WpfEnkelMiniräknare/MainWindow.xaml.cs
+++ WpfEnkelMiniräknare/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WpfEnkelMiniräknare
 {
@@ -24,5 +12,131 @@ namespace WpfEnkelMiniräknare
         {
             InitializeComponent();
         }
+
+        private void knappPlusClick(Object sender, RoutedEventArgs e)
+        {   //Flagga
+            bool ärKorrektInmattat = true;
+
+            //Töm resultat rutan
+            resultatRuta.Text = ""; 
+
+            float tal1 = 0;
+            float tal2 = 0;
+            //float tal1 = float.Parse(tal1Ruta.Text);
+            //Försöker konvertera tal 1 till en float
+            if (!float.TryParse(tal1Ruta.Text, out tal1))
+            {
+                MessageBox.Show("Tal1 fel inmatning.\n Tex 2,5");
+                ärKorrektInmattat = false;
+              
+            }      
+          // float tal2 = float.Parse(tal2Ruta.Text);
+            if (!float.TryParse(tal2Ruta.Text, out tal2))
+            {
+                MessageBox.Show("Tal2 fel inmatning.\n Tex 2,5");
+                ärKorrektInmattat = false;
+              
+            }    
+            if(ärKorrektInmattat)
+            {
+                resultatRuta.Text = $"{tal1} + {tal2} = {tal1 + tal2}";
+            } 
+
+            
+        }
+
+        private void knappMinusClick(Object sender, RoutedEventArgs e)
+        {
+           //Flagga
+            bool ärKorrektInmattat = true;
+
+            //Töm resultat rutan
+            resultatRuta.Text = ""; 
+
+            float tal1 = 0;
+            float tal2 = 0;
+            //float tal1 = float.Parse(tal1Ruta.Text);
+            //Försöker konvertera tal 1 till en float
+            if (!float.TryParse(tal1Ruta.Text, out tal1))
+            {
+                MessageBox.Show("Tal1 fel inmatning.\n Tex 2,5");
+                ärKorrektInmattat = false;
+              
+            }      
+          // float tal2 = float.Parse(tal2Ruta.Text);
+            if (!float.TryParse(tal2Ruta.Text, out tal2))
+            {
+                MessageBox.Show("Tal2 fel inmatning.\n Tex 2,5");
+                ärKorrektInmattat = false;
+              
+            }    
+            if(ärKorrektInmattat)
+            {
+                resultatRuta.Text = $"{tal1} - {tal2} = {tal1 - tal2}";
+            } 
+        }
+
+        private void knappGångerClick(Object sender, RoutedEventArgs e)
+        {
+            //Flagga
+            bool ärKorrektInmattat = true;
+
+            //Töm resultat rutan
+            resultatRuta.Text = ""; 
+
+            float tal1 = 0;
+            float tal2 = 0;
+            //float tal1 = float.Parse(tal1Ruta.Text);
+            //Försöker konvertera tal 1 till en float
+            if (!float.TryParse(tal1Ruta.Text, out tal1))
+            {
+                MessageBox.Show("Tal1 fel inmatning.\n Tex 2,5");
+                ärKorrektInmattat = false;
+              
+            }      
+          // float tal2 = float.Parse(tal2Ruta.Text);
+            if (!float.TryParse(tal2Ruta.Text, out tal2))
+            {
+                MessageBox.Show("Tal2 fel inmatning.\n Tex 2,5");
+                ärKorrektInmattat = false;
+              
+            }    
+            if(ärKorrektInmattat)
+            {
+                resultatRuta.Text = $"{tal1} * {tal2} = {tal1 * tal2}";
+            } 
+        }
+
+        private void knappDivideraClick(Object sender, RoutedEventArgs e)
+        {
+            //Flagga
+            bool ärKorrektInmattat = true;
+
+            //Töm resultat rutan
+            resultatRuta.Text = ""; 
+
+            float tal1 = 0;
+            float tal2 = 0;
+            //float tal1 = float.Parse(tal1Ruta.Text);
+            //Försöker konvertera tal 1 till en float
+            if (!float.TryParse(tal1Ruta.Text, out tal1))
+            {
+                MessageBox.Show("Tal1 fel inmatning.\n Tex 2,5");
+                ärKorrektInmattat = false;
+              
+            }      
+          // float tal2 = float.Parse(tal2Ruta.Text);
+            if (!float.TryParse(tal2Ruta.Text, out tal2))
+            {
+                MessageBox.Show("Tal2 fel inmatning.\n Tex 2,5");
+                ärKorrektInmattat = false;
+              
+            }    
+            if(ärKorrektInmattat)
+            {
+                resultatRuta.Text = $"{tal1} / {tal2} = {tal1 / tal2}";
+            } 
+        }
     }
 }
+
