61d50a15b8cce2a64ed115fba237ec2bacb1bf40
Mona Lisa <munira.ahmed@elev.ga.ntig.se>
2021-09-30 10:27:00 +0200

    for-loop och debugging

--- Labb4/MainWindow.xaml.cs
+++ Labb4/MainWindow.xaml.cs
@@ -29,23 +29,25 @@ namespace Labb4
 
           // Läs in alla rader från textfilen countries.csv
           rader = File.ReadAllLines("./Resurser/countries.csv");
+
         //Läs in sökterm
          string sökterm = rutaSökterm.Text;   
+
          if (sökterm != "")
          {
               //Hitta matchande länder 
-         foreach (var rad in rader)
+       //  foreach (var rad in rader)
+       for (int i = 1; i < rader.Length; i++)    
         {    
                  
             // Dela upp raden
-            string[] delar = rad.Split(',');
+            string[] delar = rader[i].Split(',');
 
             // Plocka ut land
             string land = delar[1];
 
             // Plocka ut landskod
             string landskod = delar[2];
-
 
                  // Hitta land som innehåller sökterm
             if (land.ToLower().Contains(sökterm.ToLower()))
@@ -54,22 +56,16 @@ namespace Labb4
 
                  rutaResultat.Text += $" {antal} {land}: {landskod} \n";
                  antal++;
-
                    // Summering
-                 rutaStatus.Text = $"Hittade {antal-1} länder.";
-                
-            }       
-            
+                 rutaStatus.Text = $"Hittade {antal-1} länder.";            
+            }          
           } 
-              rutaSökterm.Text = "";  
-           }  else
-         {
+            rutaSökterm.Text = "";  
+           } else
+            {
               rutaStatus.Text = "Ange en sökterm med bokstäver.";
                sökterm = "";
-         } 
-       
-        
-   
+            }          
     }
  
 }
