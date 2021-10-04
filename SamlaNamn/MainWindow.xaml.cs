915ac4b214905b54f206d8e3eb2f21e5b962768d
Mona Lisa <munira.ahmed@elev.ga.ntig.se>
2021-10-04 13:20:56 +0200

    Namn lista uppgift, tränat på list

--- SamlaNamn/MainWindow.xaml.cs
+++ SamlaNamn/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,6 +21,8 @@ namespace Listor
     /// </summary>
     public partial class MainWindow : Window
     {
+        //Global lista
+        List<string> namnLista = new List<string>();
         public MainWindow()
         {
             InitializeComponent();
@@ -27,11 +30,28 @@ namespace Listor
         
         private void ClickLäggTill(object sender, RoutedEventArgs e)
         {
+            
             //Läs av rutaNamn
+            string namn = rutaNamn.Text;
 
             //Om rutaNamn ej tom
             //Lägg till rutaLista
             //och Lagra i en lista
+            if (namn != "")
+            {
+                rutaLista.Text += $"{namn} \n";
+                namnLista.Add(namn);
+            }else
+            {
+                rutaFel.Text = "Namn är tomt!";
+            }
+
+            rutaNamn.Text = "";
         }
+          private void ClickSpara(object sender, RoutedEventArgs e)
+          {
+              File.WriteAllLines("namnLista.txt", namnLista);
+              rutaFel.Text = "Namnen sparade i namnLista.txt!";
+          }
     }
 }
