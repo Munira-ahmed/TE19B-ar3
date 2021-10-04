7982b17ccfeb5444ee59e597a3448c3dc591e106
Mona Lisa <munira.ahmed@elev.ga.ntig.se>
2021-10-04 12:51:05 +0200

    Listor

---
+++ ConsolLista/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+namespace ConsolLista
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //skapa en array
+            string[] städer = {"Berlin","Paris","London"};
+
+            //Ett annat sätt att skapa arrayer
+            string[] frukter = new string[3];
+            frukter[0]= "Äpple";
+            frukter[1]= "Apelsin";
+            frukter[2]= "Banan";
+
+            //Skapa en lista av namn
+            List<string> namnen = new List<string>(){"Miriam", "Teo", "Alex"};
+
+            //skapa en lista
+            List<string> bären = new List<string>();
+            bären.Add("Blåbär");
+            bären.Add("Lingon");
+            bären.Add("Hallon");
+            bären.Add("Björnbär");
+
+            //Skriv ut alla frukter
+            for (int i = 0; i < frukter.Length; i++)
+            {
+                   Console.WriteLine(frukter[i]);
+            }
+
+            // En annan loop man kan använda
+            foreach (var frukt in frukter)
+            {
+                Console.WriteLine(frukt);
+            }
+
+            //skriv alla bären i Listan
+            for (var i = 0; i < bären.Count; i++)
+            {
+                Console.WriteLine(bären[i]);
+            }
+
+            //Om vi ska hitta en stad i arrayen
+            foreach (var stad in städer)
+            {
+                if (stad == "Paris")
+                {
+                    Console.WriteLine("Paris finns i arrayen");
+                }
+            }
+
+            //Om vi vill hitta ett bär i listan
+            if (bären.Contains("Blåbär"))
+            {
+                Console.WriteLine("Blåbär finns i listan");
+            }
+
+            //Ta bort ett bär (hitta med namner)
+            bären.Remove("Hallon");
+            //eller (via index)
+            bären.RemoveAt(0);
+
+            bären.Insert(1, "Kråkbär");
+
+            foreach (var bär in bären)
+            {
+                Console.WriteLine(bär);
+            }
+
+
+        }
+    }
+}
---
+++ Uppgift7/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Uppgift7
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+       private void ClickSök(object sender, RoutedEventArgs e)
+        {
+            rutaMorsekod.Text = "";
+            //Läs in meddelandet
+            string meddelande = rutaMeddelande.Text;
+
+            
+            // Två parallella samlingar
+            string alfabetet = "ABCDEFGHIJKLMNOPQRSTUVWXYZÅÄÖ ";
+            string[] morse = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.",
+                  "....", "..", ".---", "-.-", ".-..", "--", "-.",
+                  "---", ".--.", "--.-", ".-.", "...", "-", ".--",
+                  "...-", ".--", "-..-", "-.--", "--..", ".--.-",
+                  ".-.-", "---.", " "};
+
+            //Loopa igenom meddelande bokstav för bokstav
+            foreach (var tecken in meddelande)
+            {
+                // Plocka ut bokstaven som söks
+                string bokstav = tecken.ToString().ToUpper();
+
+                // Hitta index i alfabetet (-1 betyder hittades inte)
+                int index = alfabetet.IndexOf(bokstav);
+                if (index != -1)
+                {
+                    // Sätt samman meddelandet
+                    rutaMorsekod.Text += morse[index];
+                }
+            }
+
+        }
+    }
+     
+}
